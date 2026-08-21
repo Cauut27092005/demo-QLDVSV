5add7e03954d1a6ffe9332c4ad5fa91ea49de802
Cauut27092005 <huonghuyen62@gmai.com>
2026-08-21 14:24:29 +0700

    Debug Laravel migration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-set -e
+
+echo "======================================"
+echo "Starting Laravel container..."
+echo "======================================"
 
 echo "Running Laravel migrations..."
-php artisan migrate --force
+
+php artisan migrate --force -vvv
+
+echo "Migration command finished with code: $?"
 
 echo "Starting Apache..."
+
 exec apache2-foreground
