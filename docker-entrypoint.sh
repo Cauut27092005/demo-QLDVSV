77b2b187e3054b515fd7e94e07f8abaf37083106
Cauut27092005 <huonghuyen62@gmai.com>
2026-08-24 10:30:33 +0700

    Fix Laravel production session cache

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,16 @@ echo "======================================"
 echo "Starting Laravel container..."
 echo "======================================"
 
-echo "Running Laravel migrations..."
+echo "Clearing Laravel cached configuration..."
+php artisan optimize:clear
 
+echo "Caching Laravel configuration..."
+php artisan config:cache
+
+echo "Running Laravel migrations..."
 php artisan migrate --force -vvv
 
 echo "Migration command finished with code: $?"
 
 echo "Starting Apache..."
-
 exec apache2-foreground
